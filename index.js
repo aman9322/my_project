8a23fc5db1a19a55f826de37a9d5502a8ee78ffa
boomerang121 <73784114+boomerang121@users.noreply.github.com>
2022-10-20 15:56:13 +0530

    Added algo files

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const superheroes = require('superheroes');
+
+//superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+
+var mysuperhero=superheroes.random();
+//=> 'Spider-Ham'
+console.log(mysuperhero);
